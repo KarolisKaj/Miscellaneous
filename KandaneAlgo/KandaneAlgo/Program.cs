48d7d1200912985071c4394a0f242e77d035ba21
KarolisKaj <karolis.kajenas1@gmail.com>
2017-12-02 01:14:39 +0200

    algo

---
+++ KandaneAlgo/KandaneAlgo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KandaneAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kandane.KandaneSum(new[] { -1, 3, -4, 4 }));
+            Console.Read();
+        }
+    }
+
+    public class Kandane
+    {
+        public static int KandaneSum(int[] arr)
+        {
+            int max = arr[0];
+            int currentMax = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                currentMax = Math.Max(arr[i], currentMax + arr[i]);
+                if (currentMax > max)
+                    max = currentMax;
+            }
+            return max;
+        }
+
+    }
+}
